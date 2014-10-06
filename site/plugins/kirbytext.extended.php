17b903b0e7514b033b555cda957d007e9f17b403
August W. Miller <hello@gusmiller.com>
2014-10-05 21:47:22 -0700

    Better documentation for resource shortcode

--- site/plugins/kirbytext.extended.php
+++ site/plugins/kirbytext.extended.php
@@ -6,33 +6,37 @@ class kirbytextExtended extends kirbytext {
     
     parent::__construct($text, $markdown);
     
+    # A "Resource" as we're calling it needs a filename, scope and link text:
     $this->addTags('resource');
     $this->addAttributes('page');
     $this->addAttributes('text');
                     
   }  
-
+ 
   function resource($params) {
     
+    # We need to traverse the content tree, so we reference the global $site object
     global $site;
   
-    // define default values for attributes
+    # Defaults, in case one or more params are not specified
     $defaults = array(
       'resource' => '',
       'page' => '',
       'text' => ''
     );
     
-    // merge the given parameters with the default values
+    # Make our calls failsafe, through the $options associative array
     $options = array_merge($defaults, $params);
   
-    // build the final url
+    # Make sure that both the page and filename resolve cleanly with the Kirby API:
     if ( is_object( $page = $site->pages()->find($options['page']) ) && is_object($file = $page->files()->find($options['resource']) ) ) {
-
+ 
+      # If we're good to go, return (not print) the HTML for a link.
       return '<a class="file" href="' . $file->url() . '">' . html($options['text']) . '</a>';
     } else {
+      # Otherwise, output just the text that was provided in the short-tag
       return $options['text'];
     }
   }
-
+ 
 }
