9a86286e6b030385fef8b831a600d8c02aa4c03a
August W. Miller <hello@gusmiller.com>
2014-10-05 20:59:28 -0700

    Error template

---
+++ site/templates/error.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+
+<section class="error">
+  <div class="wrapper">
+    <div class="column col-8 tablet-full">
+      <h1><?php echo html($page->title()) ?></h1>
+      <?php echo kirbytext($page->text()) ?>
+    </div>
+  </div>
+</section>
+
+<?php snippet('footer') ?>
