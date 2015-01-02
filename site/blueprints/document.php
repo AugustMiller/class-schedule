f090b468b3adf12d850653308994c9ff44868abb
August W. Miller <hello@gusmiller.com>
2015-01-02 14:14:22 -0800

    New document blueprints
    Useless (except for sorting) until panel is implemented

---
+++ site/blueprints/document.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Document
+pages: hide
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  description:
+    label: description
+    type:  textarea
---
+++ site/blueprints/documents.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Documents
+pages:
+  num: zero
+  template: document
+files: hide
+fields:
+  title:
+    label: Title
+    type:  text
+  description:
+    label: description
+    type:  textarea
