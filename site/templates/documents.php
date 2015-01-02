d466149c91f90779ddac29bb9df77d928802264c
August W. Miller <hello@gusmiller.com>
2015-01-02 14:14:47 -0800

    Cleaned up files page

--- site/templates/documents.php
+++ site/templates/documents.php
@@ -1,38 +1,34 @@
 <? snippet('header') ?>
 
-<? if ( $page->hasChildren() ) { ?>
+<section class="files-intro">
 
-  <section class="files-intro">
-
-    <div class="wrapper">
-      <div class="column col-6 tablet-full">
-        <h2>Files</h2>
-        <div class="introduction">
-          <?= kirbytext( $page->text() ) ?>
-        </div>
+  <div class="wrapper">
+    <div class="column col-6 tablet-full">
+      <h2>Files</h2>
+      <div class="introduction">
+        <?= $page->text()->kirbytext() ?>
       </div>
     </div>
+  </div>
 
-  </section>
+</section>
 
-  <section class="files">
+<section class="files">
 
-    <div class="wrapper files">
-      <? $documents = $page->children(); ?>
-      <? foreach ( $documents as $document ) { ?>
-        <? $file = $document->files()->filterBy('extension', '!=', 'txt')->first() ?>
-        <div class="column col-4 tablet-half mobile-full file">
-          <h4>
-            <a href="<?= $file->url() ?>" title="<?= $document->name() ?>" download><?= $document->title() ?></a>
-            <span class="file-type"><?= strtoupper( $file->extension() ) ?></span>
-          </h4>
-          <?= kirbytext( $document->description() ) ?>
-        </div>
-      <? } ?>
-    </div>
+  <div class="wrapper files">
+    <? $documents = $page->children()->visible(); ?>
+    <? foreach ( $documents as $document ) { ?>
+      <? $file = $document->files()->first() ?>
+      <div class="column col-4 tablet-half mobile-full file">
+        <h4>
+          <a href="<?= $file->url() ?>" title="<?= $document->name() ?>" download><?= $document->title() ?></a>
+          <span class="file-type"><?= strtoupper( $file->extension() ) ?></span>
+        </h4>
+        <?= $document->description()->kirbytext() ?>
+      </div>
+    <? } ?>
+  </div>
 
-  </section>
-
-<? } ?>
+</section>
 
 <? snippet('footer') ?>
