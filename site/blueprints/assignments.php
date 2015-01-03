81425b237f19c00faf190ba3b5d61abcf7622ec0
August W. Miller <hello@gusmiller.com>
2015-01-03 12:57:41 -0800

    Assignment template mostly complete

---
+++ site/blueprints/assignments.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Assignments
+pages:
+  template: assignment
+  num: date
+  field: date
+files: hide
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
--- site/templates/assignment.php
+++ site/templates/assignment.php
@@ -1,23 +1,37 @@
 <? snippet('header') ?>
 
-<section class="assignment">
-
+<section class="assignment-header">
   <div class="wrapper">
-    <div class="column col-12 tablet-full greedy title">
+    <div class="column col-12 tablet-full title">
       <h2>
-        <?= $assignment->title() ?>
+        <?= html::a($page->parent()->url(), "Assignment") ?>: <span class="assignment-name"><?= $page->title() ?></span>
       </h2>
-    </div>
-    <div class="column col-3 tablet-full title">
-      <div class="date">
-        Due: <?= $assignment->date() ?>
-      </div>
-    </div>
-    <div class="column col-8 tablet-full">
-      <?= $assignment->text()->kirbytext() ?>
+      <?= $page->description()->markdown() ?>
     </div>
   </div>
+</section>
 
+<section class="assignment">
+  <div class="wrapper">
+    <div class="column col-2 tablet-quarter mobile-full metadata">
+      <div class="date">
+        <strong>Due:</strong> <?= date('j F Y', $page->date()) ?>
+      </div>
+      <? /*
+      <? if ( $page->hasFiles() ) { ?>
+        <div class="attachments">
+          <ul>
+            <? foreach ( $page->files() as $file ) { ?>
+             <li><a href="<?= $file->url() ?>" title="<?= $file->name() ?>" download><?= $file->title() ?></a></li>
+            <? } ?>
+        </div>
+      <? } ?>
+      */ ?>
+    </div>
+    <div class="column col-7 tablet-three-quarters mobile-full">
+      <?= $page->text()->kirbytext() ?>
+    </div>
+  </div>
 </section>
 
 <? snippet('footer') ?>
